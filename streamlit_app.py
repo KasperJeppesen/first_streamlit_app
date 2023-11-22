966f13870b4996e42589aecc2d968273d48180f6
KasperJeppesen <151635097+KasperJeppesen@users.noreply.github.com>
2023-11-22 14:51:01 +0100

    Update streamlit_app.py
    Changed line of codes

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,6 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
 
 
 streamlit.header('Breakfast Menu')
@@ -41,3 +39,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
